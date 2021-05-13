ced18fe2e3e0012ff5deecf4f89c711c20c3dd93
Haziel <mariohaziel@gmail.com>
2021-05-13 16:45:19 -0600

    Updating Models

---
+++ BackEnd/PostgreSQLBackEnd/BackEndData/InterfaceRepositories/IAppDevice.cs
@@ -0,0 +1,16 @@
+﻿using BackEndModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndData.InterfaceRepositories
+{
+    public interface IAppDevice
+    {
+        Task<IEnumerable<AppDevice>> GetAllAppDevices();
+        Task<AppDevice> GetAppDevice(int serialNumber);
+        Task<bool> InsertAppDevice(AppDevice device);
+    }
+}
