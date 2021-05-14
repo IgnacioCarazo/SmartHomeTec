b0dba47da73758dfca8526425c1b9c00e1a1c2f7
Haziel <mariohaziel@gmail.com>
2021-05-14 15:30:41 -0600

    Agrego Scripts DB

---
+++ BackEnd/DataBase/scriptDistributor.sql
@@ -0,0 +1,43 @@
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (435, 'Importadora Monge', 'America', 'Costa Rica');
+	
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (82, 'LG', 'America', 'Costa Rica');
+	
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (8742, 'Apple', 'America', 'Estados Unidos');
+
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (852, 'BestBuy', 'America', 'Estados Unidos');
+
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (732, 'Marshall', 'America', 'Canada');
+	
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (935, 'Sony', 'Asia', 'Japon');
+	
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (762, 'Toshiba', 'Asia', 'Japon');
+	
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (543, 'Ericsson', 'Europa', 'Suecia');
+
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (5423, 'SAP', 'Europa', 'Alemania');
+
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (1343, 'Accenture', 'Europa', 'Irlanda');
+
+INSERT INTO public."Distributor"(
+	dni, name, continent, country)
+	VALUES (981, 'TechLab', 'Africa', 'Lagos');
