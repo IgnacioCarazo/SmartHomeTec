f9f9e0652545474fea0533dac9ffde1e51818ebd
Haziel <mariohaziel@gmail.com>
2021-05-15 19:20:31 -0600

    Update DB

--- BackEnd/DataBase/scriptDevices.sql
+++ BackEnd/DataBase/scriptDevices.sql
@@ -1,7 +1,7 @@
 INSERT INTO public."Device"(
 	name,"eConsumption", brand, associated, "typeName", "ownerEmail", "dniDistributor", price)
-	VALUES ('Iphone XS MAX', '11 kw/h', 'Apple', false, 'SmartPhone', 'mariohaziel@hotmail.com', 82, 873);
+	VALUES ('Iphone XS MAX', '11 kw/h', 'Apple', false, 'SmartPhone', '', 82, 873);
 
 INSERT INTO public."Device"(
 	name,"eConsumption", brand, associated, "typeName", "ownerEmail", "dniDistributor", price)
-	VALUES ('Samsung HD', '17 kw/h', 'Samsung', false, 'SmarTV', 'rovira2799@hotmail.com', 435, 1623);
+	VALUES ('Samsung HD', '17 kw/h', 'Samsung', false, 'SmarTV', '', 435, 1623);
